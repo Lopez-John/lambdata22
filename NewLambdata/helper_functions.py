c1f59789523daef75860c1e2025cb96be11b27ef
Lopez-John <johncarlolopez.88@gmail.com>
2021-01-12 16:33:08 -0500

    finally got upload to testpypi working, added new OOP examples and converted some helper functions into methods

---
+++ NewLambdata/helper_functions.py
@@ -0,0 +1,67 @@
+"""NewLambdata - A collection of Data Science Helper functions"""
+
+import pandas as pd
+import numpy as np
+from datetime import datetime as dt
+
+
+class NewDataFrame(DataFrame):
+    def nullcount(self):
+        return self.isnull().sum().sum()
+
+    def list_2_series(self, list):
+        new_series = pd.Series(list)
+        self['list'] = new_series
+        return self
+
+    def split_dates(self):
+        self['date'] = self['date'].apply(pd.to_datetime)
+        self['Month'] = self['date'].dt.month
+        self['Day'] = self['date'].dt.day
+        self['Year'] = self['date'].dt.Year
+        return self
+
+
+def null_count(df):
+    """Checks the Dataframe for null values and
+    returns amount of null values"""
+
+    return df.isnull().sum().sum()
+
+
+def list_2_series(list, df):
+    """Converts a list into a Series, and creates a new column in
+    the DataFrame"""
+
+    # converts list into pandas Series
+    new_series = pd.Series(list)
+
+    # adds Series as column in the inputed DataFrame
+    df['list'] = new_series
+    return df
+
+
+def split_dates(date_series):
+    """Function to split dates of format "MM/DD/YYYY" into multiple
+    columns (df['month'], df['day'], df['year']) and
+    then return the same dataframe with those additional columns"""
+
+    # converts dates to datetime
+    date_series['date'] = date_series["date"].apply(pd.to_datetime)
+
+    # creates a month, day, and year column
+    date_series['Month'] = date_series['date'].dt.month
+    date_series['Day'] = date_series['date'].dt.day
+    date_series['Year'] = date_series['date'].dt.year
+
+    return date_series
+
+
+def train_test_split(df, frac):
+    """Create a Train/Test split function for a dataframe and returns
+    both the Training and Testing sets. Frac referes to the precent
+    of data you would like to set aside for training"""
+
+    train_split = df[: (int(len(df) * frac))]
+    test_split = df[(int(len(df) * frac)):]
+    return train_split, test_split
---
+++ NewLambdata/oop_example.py
@@ -0,0 +1,78 @@
+"""Object Oriented Programming Examples - Sprint 1 Module 2"""
+
+
+class BareMiniumumClass:
+    pass
+
+class Complex:
+    def __init__(self, real_part, imag_part):
+        """
+        Constructor for Complex Numbers.
+        Complex numbers have a real park and imaginary part.
+        """
+        self.r = real_part
+        self.i = imag_part
+    
+    def add(self, other_complex):
+        self.r += other_complex.r
+        self.i += other_complex.i
+
+    def __repr__(self):
+        """
+        Used to show what string to print out
+        when the object's name is called itself
+        """
+
+        return "({}, {})".format(self.r, self.i) 
+
+class SocialMediaUser:
+    def __inti__(self, name, location, upvotes=0):
+        self.name = str(name)
+        self.location = location
+        self.upvotes = int(upvotes)
+
+    def receive_upvotes(self, num_upvotes=1):
+        self.upvotes = += num_upvotes
+
+    def is_popular(self): # convention for methods starting with 'is' will return a true or false value
+        return self.upvotes > 100
+
+class Animal:
+    """General Representation of Animals"""
+    def __init__(self, name, weight, diet_type):
+        self.name = str(name)
+        self.weight = float(weight)
+        self.diet_type = diet_type
+
+    def run(self):
+        return 'Vroom, Vroom, I go quick'
+    
+    def eat(self, food):
+        return 'Huge fan of that ' + food
+
+class Sloth(Animal): # class inheritence
+    pass
+
+
+
+if __name__ == "__main__":
+    """
+    only executes when the .py file is used,
+    not when the module is import
+    """
+    num1 = Complex(3, -5) # num1.r = 3, num1.i = -5
+    num2 = Complex(2, 6) # num2.r = 2, num2.i = 6
+
+    num1.add(num2)
+    print("num1. r: {} , num1.i: {} ".format(num1.r, num1.i))
+
+    user1 = SocialMediaUser('John', 'LA', 500)
+    user2 = SocialMediaUser('Carl', 'Mississippi')
+    user3 = SocialMediaUser('George Washington', 'Dijibouti', 5000)
+    user4 = SocialMediaUser('Carlos', 'Argentina', 5)
+
+    pirnt(user1.is_popular())
+    print(user2.is_popular())
+    user2.receive_upvotes(101)
+    print('received {} upvaltes'.format(user2.upvotes))
+    print(user2.is_popular())
